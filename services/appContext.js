fcd4b7e81fd67d21db61c85e741e89af34e6605b
Liam Strang <liam@liamstrang.com>
2022-04-17 10:11:06 +1000

    updated comments

--- services/appContext.js
+++ services/appContext.js
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 
+/**
+@author Liam Strang - 45426392
+@description Creates the context for the app which passes state down to the components
+*/
 export const AppContext = createContext(null);
