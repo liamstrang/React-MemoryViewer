2b95095f598bfcac7f7701ddf611f125ebf8e167
Liam Strang <strangliam0@gmail.com>
2022-05-17 00:25:57 +1000

    Added login test

--- screens/__tests__/Homepage.test.js
+++ screens/__tests__/Homepage.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../Home'
 import Registration from '../Registration'
-
-const users = require("./users/users.json");
+import Login from '../Login'
 
 describe('Check Home Page', () => {
     it('Render Homepage', () => {
@@ -24,5 +23,30 @@ describe('Check Register Page', () => {
     it('Render Register Page', () => {
         const tree = renderer.create(<Registration />).toJSON();
         expect(tree.children[2].type).toBe('View')
+    })
+    it('Register Styling', () => {
+        const json = renderer.create(<Registration/>).toJSON()
+        expect(json.props.style[0].width).toBe('100%')
+        expect(json.props.style[0].flex).toBe(1)
+        expect(json.props.style[0].maxWidth).toBe(340)
+        expect(json.props.style[0].padding).toBe(20)
+        expect(json.props.style[0].alignItems).toBe('center')
+        expect(json.props.style[0].justifyContent).toBe('center')
+    })
+})
+
+describe('Check Login Page', () => {
+    it('Render Login Page', () => {
+        const tree = renderer.create(<Login />).toJSON();
+        expect(tree.children[2].type).toBe('View')
+    })
+    it('Login Styling', () => {
+        const json = renderer.create(<Login/>).toJSON()
+        expect(json.props.style[0].width).toBe('100%')
+        expect(json.props.style[0].flex).toBe(1)
+        expect(json.props.style[0].maxWidth).toBe(340)
+        expect(json.props.style[0].padding).toBe(20)
+        expect(json.props.style[0].alignItems).toBe('center')
+        expect(json.props.style[0].justifyContent).toBe('center')
     })
 })
